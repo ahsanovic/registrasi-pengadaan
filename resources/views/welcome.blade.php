43c591f139ba9dd412538029943f5e1f8f032f4f
ahsan <ahsanovic_7@yahoo.com>
2026-03-06 08:13:38 +0700

    change asset access method

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,8 @@
   <!-- end::GXON Mobile Specific -->
 
   <!-- begin::GXON Favicon Tags -->
-  <link rel="icon" type="image/png" href="assets/images/favicon.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/apple-touch-icon.png') }}">
   <!-- end::GXON Favicon Tags -->
 
   <!-- begin::GXON Google Fonts -->
@@ -35,16 +35,16 @@
   <!-- end::GXON Google Fonts -->
 
   <!-- begin::GXON Required Stylesheet -->
-  <link rel="stylesheet" href="assets/libs/flaticon/css/all/all.css">
-  <link rel="stylesheet" href="assets/libs/lucide/lucide.css">
-  <link rel="stylesheet" href="assets/libs/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="assets/libs/simplebar/simplebar.css">
-  <link rel="stylesheet" href="assets/libs/node-waves/waves.css">
-  <link rel="stylesheet" href="assets/libs/bootstrap-select/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/libs/flaticon/css/all/all.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/libs/lucide/lucide.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/libs/fontawesome/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/libs/simplebar/simplebar.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/libs/node-waves/waves.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap-select/css/bootstrap-select.min.css') }}">
   <!-- end::GXON Required Stylesheet -->
 
   <!-- begin::GXON CSS Stylesheet -->
-  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
   <!-- end::GXON CSS Stylesheet -->
 
 </head>
@@ -55,9 +55,9 @@
     <div class="auth-cover-wrapper">
       <div class="row g-0">
         <div class="col-lg-6">
-          <div class="auth-cover" style="background-image: url(assets/images/auth/auth-cover-bg.png);">
+          <div class="auth-cover" style="background-image: url({{ asset('assets/images/auth/auth-cover-bg.png') }});">
             <div class="clearfix">
-              <img src="assets/images/auth/auth.png" alt="" class="img-fluid cover-img ms-5">
+              <img src="{{ asset('assets/images/auth/auth.png') }}" alt="" class="img-fluid cover-img ms-5">
               <div class="auth-content">
                 <h1 class="display-6 fw-bold">Selamat Datang!</h1>
                 <p>Sistem Informasi Registrasi Kontrak Pengadaan adalah sistem informasi yang dirancang untuk mengelola pendaftaran kontrak pengadaan di BKD Provinsi Jawa Timur.</p>
@@ -69,8 +69,8 @@
           <div class="p-3 p-sm-5 maxw-450px m-auto auth-inner" data-simplebar>
             <div class="mb-4 text-center">
               <a href="{{ route('login') }}" aria-label="GXON logo">
-                <img class="visible-light" src="assets/images/logo-full.svg" alt="GXON logo">
-                <img class="visible-dark" src="assets/images/logo-full-white.svg" alt="GXON logo">
+                <img class="visible-light" src="{{ asset('assets/images/logo-full.svg') }}" alt="GXON logo">
+                <img class="visible-dark" src="{{ asset('assets/images/logo-full-white.svg') }}" alt="GXON logo">
               </a>
             </div>
             <div class="text-center mb-5">
@@ -123,9 +123,9 @@
 
   </div>
   <!-- begin::GXON Page Scripts -->
-  <script src="assets/libs/global/global.min.js"></script>
-  <script src="assets/js/appSettings.js"></script>
-  <script src="assets/js/main.js"></script>
+  <script src="{{ asset('assets/libs/global/global.min.js') }}"></script>
+  <script src="{{ asset('assets/js/appSettings.js') }}"></script>
+  <script src="{{ asset('assets/js/main.js') }}"></script>
   <script>
     window.addEventListener('pageshow', function (event) {
       if (event.persisted) {
